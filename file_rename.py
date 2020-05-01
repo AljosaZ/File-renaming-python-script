8e7ca3b828f5e23ad8b64e0089c76f7c8b124c2a
AljosaZ <znidaric.aljosa@gmail.com>
2020-05-01 14:23:10 +0200

    put the integer_part +=1 at the end of for loop, to shorten code

--- file_rename.py
+++ file_rename.py
@@ -6,13 +6,12 @@ string_part = str(input('Enter the text part of the new file name (it will stay 
 integer_part = int(input('Enter the integer value part of the new file name (it will rise for +1 for each new file):'))
 path_input = input('Enter path for the directory in which your files are:')
 file_type = input('Enter the type of the files you want to rename (.xxx):')
-i = 0
+i = 1
 
 for file in os.listdir(path_input):
-    integer_part += i
     integer_part_formatting = f'{integer_part:04}'
     integer_part_string = str(integer_part_formatting)
     backward_slash = '\\'
     newname = f'{string_part + integer_part_string + file_type}'
     os.rename(f'{path_input + backward_slash + file}', f'{path_input + backward_slash + newname}')
-    i = 1
+    integer_part += i
